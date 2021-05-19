003128a18049dc24f0b90e6218be578112f96b8d
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-05-19 12:31:48 +0200

    Add About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,52 @@
 <template>
-  <div id="about" class="container vh-100" style="padding-top:10rem">
+  <div id="about" class="container vh-100 text-start" style="padding-top: 10rem">
     <h2>Sobre nosotros</h2>
+    <strong
+      >Campus Latino - Todos los domingos de 11:00 a.m. a 2:30 p.m.</strong
+    >
+    <br/>
+    <br/>
+    <p>
+      Campus Latino es el programa latinoamericano de Radio Campus creado en
+      1980 en el contexto de programas dirigidos a comunidades extranjeras en
+      Bruselas. Emitiendo en el 92.1 de la banda FM, Radio Campus es la Radio
+      Comunitaria de la Universidad Libre de Bruselas. El objetivo principal es
+      dirigirse al público no latinoamericano con el mismo entusiasmo que la
+      comunidad latinoamericana.
+    </p>
+
+    <p>
+      Campus Latino es una organización que promueve la difusión de música,
+      información y busca dar a conocer a los Pueblos de América.
+    </p>
+
+    <p>
+      Es decir, que el espectáculo no se detiene solo en el folclore o la
+      variedad del mundo latinoamericano. Participa en la difusión de elementos
+      que ayuden a expresar las identidades latinoamericanas en solidaridad con
+      la lucha de todos los pueblos del mundo por evitar los estereotipos.
+    </p>
+
+    <p>
+      Campus Latino debe defender:<br />
+      el derecho a la vida;<br />
+      el derecho a una cultura indígena;<br />
+      el derecho a acceder a los recursos naturales;<br />
+      respeto por las minorías;<br />
+      reconocimiento y acceso a los derechos políticos, económicos, sociales,
+      culturales y religiosos;<br />
+      valores (que deben expresarse sin omitir la pertenencia a una identidad
+      indígena básica y herederos de la Declaración Universal de Derechos
+      Humanos y de los Pueblos).<br />
+    </p>
+
+    <p>
+      Campus Latino está al servicio de todos aquellos que quieran denunciar
+      cualquier infracción o acto contrario a los derechos universales y
+      mantendrá en todo caso su independencia de cualquier partido político,
+      corriente ideológica o religiosa y se regirá por los principios de
+      libertad e igualdad social...
+    </p>
   </div>
 </template>
 
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="welcome" class="p-5 vh-100">
+  <div id="welcome" class="vh-100">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
     <strong>La radio de la comunidad latinoamericana en Bruselas</strong>
@@ -13,6 +13,7 @@
         >Facebook</a
       >.
     </p>
+    <img height="500px" src="https://scontent.fcrl1-1.fna.fbcdn.net/v/t1.6435-9/179483261_3987581138024506_8998535709353194794_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=973b4a&_nc_ohc=OF7pSr4JTiAAX81D03i&_nc_ht=scontent.fcrl1-1.fna&oh=167ceff859d600f4654d83df0b7109b8&oe=60CC44C6" alt="Banner">
   </div>
 </template>
 
