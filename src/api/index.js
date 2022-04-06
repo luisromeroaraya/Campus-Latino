2d05ef0b22b9f02dffad2c201975ef8137106ad3
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-04-06 22:22:49 +0200

    Fixed getEpisodes to filter only podcast from campus-latino

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,9 @@ export default function getEpisodes(URL, episodes) {
         .then(res => res.json())
         .then(json => {
             json.data.forEach( function (element) {
-                episodes.push(element);
+                if (element['key'].includes('campus-latino-')) {
+                    episodes.push(element);
+                }
             });
             episodes.sort((a, b) => (a.name > b.name) ? -1 : 1)
             if (json.paging.next) {
